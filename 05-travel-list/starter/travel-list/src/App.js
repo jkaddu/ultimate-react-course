b25ab06ab8421a5dcaa809f3851add9b9f18a941
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-28 10:36:22 -0500

    flashcard component challenege done

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -2,6 +2,7 @@ import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
+import FlashCards from "./components/FlashCards";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Logo />
       <Form />
       <PackingList />
+      <FlashCards />
       <Stats />
     </div>
   );
