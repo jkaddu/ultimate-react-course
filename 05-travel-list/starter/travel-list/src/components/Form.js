c52368e4a7e77a3b151f460457d02d2550cf9560
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-28 09:51:09 -0500

    added state to form for input/select elements

--- 05-travel-list/starter/travel-list/src/components/Form.js
+++ 05-travel-list/starter/travel-list/src/components/Form.js
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    // Guard clause, if nothing typed in input form can't be submitted
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       {/* Creates an array of 20 elements aka numbers, "_" is the current value and "i" is the index */}
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
