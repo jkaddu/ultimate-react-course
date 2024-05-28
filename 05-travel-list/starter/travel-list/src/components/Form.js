8f950cd2309cd290fbf1cca4b9f2f21f78d4cd38
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-27 21:16:13 -0500

    added form structure to Form file

--- 05-travel-list/starter/travel-list/src/components/Form.js
+++ 05-travel-list/starter/travel-list/src/components/Form.js
@@ -1,7 +1,20 @@
 export default function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      {/* Creates an array of 20 elements aka numbers, "_" is the current value and "i" is the index */}
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
--- 05-travel-list/starter/travel-list/src/components/PackingList.js
+++ 05-travel-list/starter/travel-list/src/components/PackingList.js
@@ -1,10 +1,10 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
+  { id: 4, description: "Socks", quantity: 12, packed: false },
+  { id: 5, description: "Socks", quantity: 12, packed: true },
+  { id: 6, description: "Socks", quantity: 12, packed: false },
 ];
 
 export default function PackingList() {
@@ -12,7 +12,7 @@ export default function PackingList() {
     <div className="list">
       <ul className="list">
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
