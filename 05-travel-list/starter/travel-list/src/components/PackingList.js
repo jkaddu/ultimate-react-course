94519ddb8bd70ba310b88206ebfc5104f23147d6
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-14 22:03:03 -0500

    created a sort feature using derived state in PackingList component

--- 05-travel-list/starter/travel-list/src/components/PackingList.js
+++ 05-travel-list/starter/travel-list/src/components/PackingList.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 function PackingList({ items, handleDelete, handleToggle }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  console.log(sortedItems);
   return (
     <div className="list">
       <ul className="list">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -11,6 +30,14 @@ function PackingList({ items, handleDelete, handleToggle }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
