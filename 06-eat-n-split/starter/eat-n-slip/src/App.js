55a1045d330693b74362fbd90e1eaa552905c93c
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-15 18:13:16 -0500

    add a friend feature working

--- 06-eat-n-split/starter/eat-n-slip/src/App.js
+++ 06-eat-n-split/starter/eat-n-slip/src/App.js
@@ -3,6 +3,27 @@ import FormAddFriend from "./components/FormAddFriend";
 import FriendsList from "./components/FriendsList";
 import { useState } from "react";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 function Button({ children, onClick }) {
   return (
     <button className="button" onClick={onClick}>
@@ -13,16 +34,24 @@ function Button({ children, onClick }) {
 
 export default function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
   }
 
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList Button={Button} />
-        {showAddFriend && <FormAddFriend Button={Button} />}
+        <FriendsList Button={Button} friends={friends} />
+        {showAddFriend && (
+          <FormAddFriend Button={Button} handleAddFriend={handleAddFriend} />
+        )}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
