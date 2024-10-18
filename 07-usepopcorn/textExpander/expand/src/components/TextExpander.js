4d3e6bd15f653861bb47b3bf9d6216f89ab90e24
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-17 21:27:54 -0500

    TextExpander done

--- 07-usepopcorn/textExpander/expand/src/components/TextExpander.js
+++ 07-usepopcorn/textExpander/expand/src/components/TextExpander.js
@@ -5,7 +5,7 @@ export default function TextExpander({
   collaspedNumWords = 10,
   expandButtonText = "Show more",
   collaspedButtonText = "Show less",
-  buttonColor = "",
+  buttonColor = "blue",
   className = "",
   children,
 }) {
@@ -13,11 +13,24 @@ export default function TextExpander({
   function handleExpand() {
     setIsExpanded(!isExpanded);
   }
-  const displayText = isExpanded ? children : "test";
+
+  const displayText = isExpanded
+    ? children
+    : children.split(" ").slice(0, collaspedNumWords).join(" ") + "...";
+
+  const buttonStyle = {
+    backGround: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    color: buttonColor,
+    marginLeft: "4px",
+  };
+
   return (
     <div className={className}>
       <span>{displayText}</span>
-      <button color={buttonColor} onClick={handleExpand}>
+      <button style={buttonStyle} onClick={handleExpand}>
         {isExpanded ? collaspedButtonText : expandButtonText}
       </button>
     </div>
