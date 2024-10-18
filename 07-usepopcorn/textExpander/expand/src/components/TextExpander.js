5f930effff43aba3115a2c222abc3501d1289674
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-17 21:14:40 -0500

    added almost all the props for the TextExpander component

--- 07-usepopcorn/textExpander/expand/src/components/TextExpander.js
+++ 07-usepopcorn/textExpander/expand/src/components/TextExpander.js
@@ -1,3 +1,25 @@
-export default function TextExpander() {
-  return <div>TODO</div>;
+import { useState } from "react";
+
+export default function TextExpander({
+  expanded = false,
+  collaspedNumWords = 10,
+  expandButtonText = "Show more",
+  collaspedButtonText = "Show less",
+  buttonColor = "",
+  className = "",
+  children,
+}) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+  function handleExpand() {
+    setIsExpanded(!isExpanded);
+  }
+  const displayText = isExpanded ? children : "test";
+  return (
+    <div className={className}>
+      <span>{displayText}</span>
+      <button color={buttonColor} onClick={handleExpand}>
+        {isExpanded ? collaspedButtonText : expandButtonText}
+      </button>
+    </div>
+  );
 }
