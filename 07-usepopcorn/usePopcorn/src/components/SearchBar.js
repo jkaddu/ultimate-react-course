f2cf9ec79cd154ee40575cd9ee4acfcecc84b746
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-25 00:11:45 -0500

    used the useRef hook to select and focus on a DOM element in SearchBar component

--- 07-usepopcorn/usePopcorn/src/components/SearchBar.js
+++ 07-usepopcorn/usePopcorn/src/components/SearchBar.js
@@ -1,4 +1,28 @@
+import { useEffect, useRef } from "react";
+
 export default function SearchBar({ query, setQuery }) {
+  // The React way of selecting and using a DOM element
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+
+    function enterKeyCallBack(e) {
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    }
+    document.addEventListener("keydown", enterKeyCallBack);
+    return document.addEventListener("keydiwn", enterKeyCallBack);
+  }, []);
+
+  // Older way aka javaScript way of selecting and using a DOM element
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
+
   return (
     <input
       className="search"
@@ -6,6 +30,7 @@ export default function SearchBar({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
