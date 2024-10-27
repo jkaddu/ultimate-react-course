beeaab39e953069b392ffd6c956e37a16c32af88
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-27 15:26:10 -0500

    added styling using a ternary to address specific cases

--- 10-react-quiz/starter/quiz/src/components/App.js
+++ 10-react-quiz/starter/quiz/src/components/App.js
@@ -22,15 +22,17 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      return { ...state, answer: action.payload };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
-    reducer,
-    initialState
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    initialState,
+    reducer
   );
 
   const numQuestions = questions.length;
@@ -51,7 +53,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
