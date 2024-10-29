47dc606018edb2a849e48a33f91fb8f02ab0c7c8
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-28 20:56:55 -0500

    created a restart case for the rest button in the FinisCcreen component

--- 10-react-quiz/starter/quiz/src/components/App.js
+++ 10-react-quiz/starter/quiz/src/components/App.js
@@ -15,6 +15,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -37,16 +38,30 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.higscore,
+      };
+    case "restart":
+      return {
+        ...state,
+        status: "ready",
+        questions: state.questions,
+        index: 0,
+        answer: null,
+        points: 0,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
@@ -82,11 +97,21 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
         )}
         {status === "finished" && (
-          <FinishScreen points={points} maxPoints={maxPoints} />
+          <FinishScreen
+            points={points}
+            maxPoints={maxPoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
--- 10-react-quiz/starter/quiz/src/components/FinishScreen.js
+++ 10-react-quiz/starter/quiz/src/components/FinishScreen.js
@@ -1,12 +1,32 @@
 import React from "react";
 
-export default function FinishScreen({ points, maxPoints }) {
+export default function FinishScreen({
+  points,
+  maxPoints,
+  highscore,
+  dispatch,
+}) {
   const percentage = (points / maxPoints) * 100;
 
+  let emoji;
+  if (percentage === 100) emoji = "🏆";
+  if (percentage >= 80) emoji = "🙂";
+  if (percentage >= 50) emoji = "😕";
+  if (percentage >= 40) emoji = "😔";
+
   return (
-    <div className="result">
-      You scored <strong>{points}</strong> out of {maxPoints}! (
-      {Math.ceil(percentage)}%)
-    </div>
+    <>
+      <p className="result">
+        {emoji} You scored <strong>{points}</strong> out of {maxPoints}. (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore is {highscore} points.)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
   );
 }
