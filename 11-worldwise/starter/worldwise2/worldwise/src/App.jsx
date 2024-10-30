710ff0449c7a0dd34faaf13a5cdd4bdad4b7403e
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-30 11:59:02 -0500

    passed in cities and isLoading as props to CityList component

--- 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
@@ -37,8 +37,14 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
