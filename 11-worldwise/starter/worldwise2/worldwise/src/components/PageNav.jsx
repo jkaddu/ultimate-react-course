acacaaa3a1f0f59e86935abe7fe926053d7e7825
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-29 20:01:47 -0500

    added NavLink for Login jsx page in PageNav jsx component

--- 11-worldwise/starter/worldwise2/worldwise/src/components/PageNav.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/components/PageNav.jsx
@@ -6,13 +6,13 @@ export default function PageNav() {
     <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
           <NavLink to="/product">Product</NavLink>
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
