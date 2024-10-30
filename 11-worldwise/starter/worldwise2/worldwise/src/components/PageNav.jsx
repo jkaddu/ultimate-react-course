8de1bf0c4505c8a49d637521a9520782ee28b724
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-29 20:06:59 -0500

    added Logo jsx component to PageNav component

--- 11-worldwise/starter/worldwise2/worldwise/src/components/PageNav.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/components/PageNav.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 export default function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
         <li>
           <NavLink to="/product">Product</NavLink>
