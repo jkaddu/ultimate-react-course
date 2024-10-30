5b6c7053cfe86e8b169a096069b1d33fa6f3ecfb
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-30 11:56:43 -0500

    craeted state for cities and isLoading in App file/useEffect that fetches city data

--- 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
@@ -6,8 +6,30 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const URL = "http://localhost:9000";
 
 export default function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
