1170693ffe9e6a8389a501829da3714b9d3c8fbd
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-30 22:19:16 -0500

    using useParams to pass info

--- 11-worldwise/starter/worldwise2/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/components/Map.jsx
@@ -1,5 +1,17 @@
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  console.log(lat, lng);
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </div>
+  );
 }
