2f7e45aad46c84d0a5c75d8c81d46c1697bcaa8a
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-30 21:28:13 -0500

    created new Route in App file to store state in URL

--- 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const URL = "http://localhost:9000";
 
@@ -46,6 +47,7 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
