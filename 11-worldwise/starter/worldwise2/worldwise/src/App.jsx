4add766a3cfe0b98c60a7373d936f80084b87890
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-10-31 10:17:10 -0500

    implemented navigate for routing

--- 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise2/worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Homepage from "./pages/Homepage";
@@ -41,10 +41,7 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
